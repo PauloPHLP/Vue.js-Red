5fa3d73ab3e9301f77759a47b2b1cf7f1e557bad
PauloPHLP <plima.himself@gmail.com>
2022-02-08 10:09:40 -0300

    Feature: Add Webpack config.

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+declare global {
+  interface Window {
+    mountRed: Function
+    unmountRed: Function
+  }
+}
+
+window.mountRed = (containerId: string) => {
+  createApp(App).use(router).mount(`#${containerId}`)
+}
+
+window.unmountRed = (containerId: string) => {
+  const container = document.getElementById(`#${containerId}`)
+
+  if (container) document.removeChild(container)
+}
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+const { plugins } = require('./plugins')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [...plugins, new VueLoaderPlugin()],
+    devServer: {
+      historyApiFallback: true,
+      watchOptions: { aggregateTimeout: 300, poll: 1000 },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+      }
+    }
+  }
 }
